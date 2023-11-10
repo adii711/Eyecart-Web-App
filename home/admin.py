8d9e28e4fe906a7f33787f1b6a5964c953528a47
Aditya Singh <78498965+adii711@users.noreply.github.com>
2023-11-10 11:32:24 +0530

    Create admin.py

---
+++ home/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from home.models import YourModel,CartItem,newform
+admin.site.register(YourModel),
+admin.site.register(CartItem),
+admin.site.register(newform),
+# Register your models here.
